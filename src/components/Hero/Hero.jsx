f7ce37df132eded46545c77eef0f2fa7c38eeca8
MenassieErmias <ermiasmenase@gmail.com>
2024-07-08 10:51:45 +0300

    hero, left section content

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './hero.css'
+
+const Hero = () => {
+  return (
+    <main className='hero'>
+      <div className="hero-container">
+        <section className="section-left">
+          <h1 className="section-header">
+            Yamato Engineering
+          </h1>
+          <h2 className="section-subheading">
+            Empowering the future of water
+          </h2>
+          <p className="section-paragraph">
+            At Yamato Engineering plc, we take pride in offering a comprehensive range of services tailored to meet the specific needs of our clients in the fields of Control Boards, Solar Power, and SCADA Water Distribution Systems. With our expertise and dedication to innovation, we strive to provide sustainable, efficient, and reliable solutions that drive success for businesses and industries worldwide.
+          </p>
+        </section>
+        <seciton className="section-right">
+
+        </seciton>
+      </div>
+    </main>
+  )
+}
+
+export default Hero
